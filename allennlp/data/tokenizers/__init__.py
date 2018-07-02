ece183622db2657fa86d9525d3e8c30e5d190841
Mokanarangan Thayaparana <mokanarangan@gmail.com>
2018-07-02 11:27:16 +0530

    feat(sinhala): sinhala tokenizer added

--- allennlp/data/tokenizers/__init__.py
+++ allennlp/data/tokenizers/__init__.py
@@ -6,4 +6,3 @@ tokenization, stemming, and filtering.
 from allennlp.data.tokenizers.tokenizer import Token, Tokenizer
 from allennlp.data.tokenizers.word_tokenizer import WordTokenizer
 from allennlp.data.tokenizers.character_tokenizer import CharacterTokenizer
-from allennlp.data.tokenizers.sinhala_character_tokenizer import SinhalaCharacterTokenizer
